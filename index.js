bc0d46ac7b819ddbc9eeb62f6d5b325d0d87abc6
Atai Chynaliev <chynaliev.ae@gmail.com>
2021-12-14 22:02:17 +0600

    wip

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const keys = require("./config/key");
 const app = express();
 
 passport.use(new GoogleStrategy());
