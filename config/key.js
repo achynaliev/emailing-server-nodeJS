cea007c592e029040174898820689d2820c76b67
Atai Chynaliev <chynaliev.ae@gmail.com>
2021-12-14 21:59:47 +0600

    adding google oauth and config

---
+++ config/key.js
@@ -0,0 +1,5 @@
+module.exports = {
+  googleClientID:
+    "419133442550-luj3uptpvsnvtka566vs73e3jr3eu5jv.apps.googleusercontent.com",
+  googleClientSecret: "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",
+};
